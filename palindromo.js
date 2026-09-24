c852ee0752bfcd8df52f14d8e1aaa6556338ef20
Jorge Emilio <jorgegarciarojasmunoz@gmail.com>
2026-09-24 14:51:48 -0600

    programa palindromo

---
+++ palindromo.js
@@ -0,0 +1,19 @@
+function palindromo(palabra){
+
+    let izquierda = 0;
+    let derecha = palabra.length-1;
+    palabra = palabra.toLowerCase();
+
+    while(izquierda < derecha){
+        if(palabra[izquierda]!== palabra[derecha]){
+            return false
+        }
+        izquierda++;
+        derecha--;
+    }
+    return true;
+
+}
+
+console.log(palindromo('Ama'));
+console.log(palindromo('tinkyWinky'));
