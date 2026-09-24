8a17b1456b7b8c0bd80c2cfa410c38f95f19349f
Jorge Emilio <jorgegarciarojasmunoz@gmail.com>
2026-09-24 14:37:47 -0600

    eliminamos palindromo duplicado

---
+++ fizzBuzz.js
@@ -0,0 +1,19 @@
+function fizzBuzz(){
+    for(let n = 1; n <=100;n++){
+        if(n % 3 === 0 && n % 5 === 0){
+            console.log('fizzBuzz');
+        }
+        else if(n % 3 === 0){
+            console.log('fizz');
+        }
+        else if(n % 5 === 0){
+            console.log('Buzz');
+        }
+
+        else{
+            console.log(n);
+        }
+    }
+}
+
+fizzBuzz();
